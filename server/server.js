b4f549e1d2963ed64d2fc6850f1fe441eb14e671
nmisustin <nmisustin@gmail.com>
2021-07-07 14:35:52 -0400

    lesson 3

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const {ApolloServer} = require('apollo-server-express');
 const {typeDefs, resolvers} = require('./schemas');
 const db = require('./config/connection');
 const {authMiddleware} = require('./utils/auth');
+const path = require('path');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -15,6 +16,13 @@ server.applyMiddleware({app});
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+})
 
 db.once('open', () => {
   app.listen(PORT, () => {
